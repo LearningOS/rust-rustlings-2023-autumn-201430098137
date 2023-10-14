1bcd1c88a36b4112f6d0e62d52c4d82c9c8ecf07
201430098137 <zhuangg@91160.com>
2023-10-14 13:21:36 +0800

    feat:finied 99%

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,28 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let ss:Vec<&str> = s.split(",").collect();
+        if ss.len() != 2 {
+            return Person::default();
+        }else if ss[0] == "" {
+            return Person::default();
+        }else if ss[1].parse::<usize>().is_err(){
+            return Person::default();
+        }else {
+            return Person{
+                name:ss[0].to_string(),
+                age:ss[1].parse::<usize>().unwrap()
+            }
+        }
+
+
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,23 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let ss:Vec<&str> = s.split(",").collect();
+        if ss.len() != 2 {
+            Err(ParsePersonError::BadLen)
+        }else if ss[0] == "" {
+            Err(ParsePersonError::NoName)
+        }else if ss[1].parse::<usize>().is_err(){
+            Err(ParsePersonError::ParseInt(ss[1].parse::<usize>().err().unwrap()))
+        }else {
+            Ok(Person{
+                name:ss[0].to_string(),
+                age:ss[1].parse::<usize>().unwrap()
+            })
+        }
     }
 }
 
